60d4f2f0a2632d5d7c599bec7931d7262ab941c5
Vitaliy Serdyukov <vitaliy_serdyukov@hotmail.com>
2021-04-13 11:19:09 +0200

    upload

---
+++ reposTest/src/DateToday.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class DateToday {
+  public static void main(String[] args) {
+
+    Date date = new Date ();
+    System.out.println("Today is: " + date.getDate());
+
+  }
+}
